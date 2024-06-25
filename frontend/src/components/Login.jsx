986fe0bdfec6e753ca013722ae46db28ef398d49
Denis Sab <denis.sabs@ymail.com>
2024-06-25 11:28:53 +0200

    added styles and show hide password function

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,89 +1,126 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = ({setUser}) => {
-
-  
+const Login = ({ setUser }) => {
+  // useState ---------------------------------------
   const [formValues, setFormValues] = useState({
     username: "",
     password: "",
-  })
-  const [error, setError] = useState(null)
-  const[loading, setLoading] = useState(false)
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
+  // HANDLERS --------------------------------------
   const handleInput = (e) => {
     //console.log(e.target.name);
     //console.log(e.target.value)
-    setFormValues((prev) => ({...prev, [e.target.name]: e.target.value }));
-     //console.log(formValues)
-  }
+    setFormValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    //console.log(formValues)
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-      setError(null)
-      setLoading(true)
+    setError(null);
+    setLoading(true);
 
-      try {
-        const response = await fetch("http://localhost:8000/users/login", {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(formValues),
-        })
+    try {
+      const response = await fetch("http://localhost:8000/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formValues),
+      });
 
-       // if(!response.ok) throw Error("Fetching error")   --- blocking error from backend
+      // if(!response.ok) throw Error("Fetching error")   --- blocking error from backend
 
-          const data = await response.json()
-          localStorage.setItem("token", JSON.stringify(data.token))
-          
-          setUser(true)
-         // console.log(user)
-          console.log(data)
-                
-      }catch(error) {
-        setError(error.message)
-      }finally {
-        setLoading(false)
-      }
-  }
+      const data = await response.json();
+      localStorage.setItem("token", JSON.stringify(data.token));
 
+      setUser(true);
+      // console.log(user)
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // RETURN --------------------------------------------------
   return (
-
     <>
-      <form onSubmit={handleSubmit}
-        className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-
-        <h2 className="text-2xl font-bold mb-6 text-center">Log in</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-lg mx-auto mt-24 p-6 bg-white "
+      >
+        <h2 className="text-4xl font-bold mb-2 text-center">Welcome Back! </h2>
+        <p className="text-center text-xl mb-6 text-gray-600">
+          Log in below to acces your account.
+        </p>
 
         <div className="mb-4 ">
+          <label
+            htmlFor="username"
+            className="block text-gray-700 font-medium mb-2"
+          ></label>
 
-          <label htmlFor="username" 
-           className="block text-gray-700 font-medium mb-2">Username</label>
-
-          <input type="text" name="username" id="username" 
-          value={formValues.username} onChange={handleInput}
-          className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+            value={formValues.username}
+            onChange={handleInput}
+            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+          />
         </div>
 
-        <div className="mb-4 bg-zinc-100">
-
-          <label htmlFor="password"
-          className="block text-gray-700 font-medium mb-2">Password</label>
-
-          <input type="text" name="password" id="password" 
-            value={formValues.password} onChange={handleInput}
-            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-indigo-500" />
-       
+        <div className="mb-4 ">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 font-medium mb-2"
+          ></label>
+          <div className="relative ">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="password"
+              value={formValues.password}
+              onChange={handleInput}
+              className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:border-indigo-500"
+            />
+            <button
+              type="button"
+              onClick={toggleShowPassword}
+              className="absolute inset-y-0 right-0 px-3 py-2"
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          </div>
         </div>
-        
-        <button type="submit" disabled={loading}
-          className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition duration-300">Log in</button>
 
-          {error && <p className="text-red-500 mt-4">{error}</p>}
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-full bg-black text-xl text-white py-2 rounded-lg hover:bg-indigo-700 transition duration-300"
+        >
+          Login
+        </button>
+
+        <Link className="block text-center text-2xl underline mt-6 underline-offset-4 mt-10 mb-32">
+          NOT A MEMBER? JOIN HERE
+        </Link>
+
+        {error && <p className="text-red-500 mt-4">{error}</p>}
       </form>
-
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
