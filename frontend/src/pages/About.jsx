06f1730075113c55ca9f03fb3adb98e336a8e6bc
rbksy <rabiaaksoy93@gmail.com>
2024-06-27 15:57:10 +0200

    about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,26 +1,56 @@
-
+import AboutHeroImage from '../assets/about-hero.png';
+import RabiaImage from '../assets/rabia.png';
+import DenisImage from '../assets/denis.png';
+import AnithaImage from '../assets/anitha.png';
+import RoyceImage from '../assets/royce.png';
+import CreativityImage from '../assets/creativity.png';
+import CuriosityImage from '../assets/curiosity.png';
+import UtilityImage from '../assets/utility.png';
+import ContactImage from '../assets/contact.png';
+import { Link } from 'react-router-dom';
 
 
 const About = () => {
   return (
-
-      <div className="about-container">
-        <h2>About Us</h2>
-        <p>Welcome to our website!</p>
-        <p>We aim to provide comprehensive details a</p>
-        <p>Feel free to explore our website and learn more</p>
-        <h3>Our Team</h3>
-        <ul>
-          <li> Web Developer</li>
-          <li> Web Developer</li>
-          <li> Web Developer</li>
-          <li> Web Developer</li>
-        </ul>
-        <p>Contact us at: @wd_50group</p>
+    <div className="about">
+    <div className="about-hero">
+      <img className="about-hero-image" src={AboutHeroImage} alt="About Us Hero Image" />
+      <div className="about-hero-text">
+        <h2>We are</h2>
+        <p> 
+          A creative hub that empowers your DIY dreams, turning ideas into reality, one project at a time.
+        </p>
+        <Link to="/projects" className="see-projects-button">See Projects</Link>
       </div>
-
-    
-  );
-}
+    </div>
+    <div className="meet-our-team">
+      <h2>Meet Our Team</h2>
+      <div className="team-members">
+        <div className="team-member">
+          <img src={RabiaImage} alt="Rabia" />
+          <p>Rabia</p>
+          <p>Web Developer</p>
+        </div>
+        <div className="team-member">
+          <img src={DenisImage} alt="Denis" />
+          <p>Denis</p>
+          <p>Web Developer</p>
+        </div>
+        <div className="team-member">
+          <img src={AnithaImage} alt="Anitha" />
+          <p>Anitha</p>
+          <p>Web Developer</p>
+        </div>
+        <div className="team-member">
+          <img src={RoyceImage} alt="Royce" />
+          <p>Royce</p>
+          <p>Web Developer</p>
+        </div>
+      </div>
+    </div>
+    {/* Additional sections or content can be added below */}
+  </div>
+);
+};
 
 export default About;
