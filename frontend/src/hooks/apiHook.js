8267a4c442fb38bc2da610611f9ed25b19501300
roycejohn <cuevasroycejohn.com>
2024-06-28 12:51:02 +0200

    ready for deployment

--- frontend/src/hooks/apiHook.js
+++ frontend/src/hooks/apiHook.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/projects';
+// const API_URL = 'http://localhost:8000/api/projects';
+const API_URL ='https://diyconnect.onrender.com/api/projects';
 
 export const getProjects = async () => {
   try {
